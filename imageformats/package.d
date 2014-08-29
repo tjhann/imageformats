7cfba24f21a4fba7e9146d0c9e221c64994f85c6
Tero Hänninen <nomail@invalid>
2014-08-29 10:48:09 +0300

    Import jpeg in package module

--- imageformats/package.d
+++ imageformats/package.d
@@ -2,5 +2,6 @@
 // Boost Software License - Version 1.0 - August 17th, 2003
 module imageformats;
 
+public import imageformats.jpeg;
 public import imageformats.png;
 public import imageformats.tga;
